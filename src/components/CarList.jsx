0d1a1b2c3ab361242b78b8e52058d8af8dd4f0d4
divyuk <div14kes@gmail.com>
2023-09-15 00:16:24 +0530

    Completed The App

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -3,19 +3,26 @@ import { removeCar } from "../store";
 
 function CarList() {
   const dispatch = useDispatch();
-  const carsList = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter((car) =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  });
+  const { carsList, name } = useSelector(
+    ({ form, cars: { data, searchTerm } }) => {
+      const fitleredCars = data.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      return {
+        carsList: fitleredCars,
+        name: form.name,
+      };
+    }
+  );
 
   const handleDelete = (car) => {
     dispatch(removeCar(car.id));
   };
   const renderedCars = carsList.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
       <div key={car.id}>
-        <p>
+        <p style={{ fontWeight: bold ? "bold" : "normal" }}>
           {car.name} - ${car.cost}
         </p>
         <button onClick={() => handleDelete(car)}>Delete</button>
